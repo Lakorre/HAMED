7ff04ca5b38d08245a5d641f354377b94e536311
إلركس <aliahejje209@gmail.com>
2026-07-26 09:32:32 +0300

    Update site.lua

--- site.lua
+++ site.lua
@@ -5,27 +5,19 @@ local TabsBarWidth = 0
 local SectionsPadding = 10
 local MachoPaneGap = 10
 
--- حساب عرض القسم الوحيد ليأخذ المساحة المتاحة كاملة
 local EachSectionWidth = MenuSize.x - (SectionsPadding * 2)
 
 local SectionOneStart = vec2(TabsBarWidth + SectionsPadding, SectionsPadding + MachoPaneGap)
 local SectionOneEnd = vec2(SectionOneStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
 
--- إنشاء النافذة الرئيسية
 MenuWindow = MachoMenuWindow(MenuStartCoords.x, MenuStartCoords.y, MenuSize.x, MenuSize.y)
 
 MachoMenuSetAccent(MenuWindow, 137, 52, 235)
 
--- القسم الوحيد
 FirstSection = MachoMenuGroup(MenuWindow, "Main Section", SectionOneStart.x, SectionOneStart.y, SectionOneEnd.x, SectionOneEnd.y)
-
--- إضافة بعض العناصر كمثال داخل القسم الوحيد
-MenuSliderHandle = MachoMenuSlider(FirstSection, "Slider", 10, 0, 100, "%", 0, function(Value)
-    print("Slider updated with value ".. Value)
-end)
 
 InputBoxHandle = MachoMenuInputbox(FirstSection, "Input", "...")
 
-MachoMenuButton(FirstSection, "CloseEEEEEEEEEEEE", function()
+MachoMenuButton(FirstSection, "Close", function()
     MachoMenuDestroy(MenuWindow)
 end)
